e8ee4b37c62c280ad47e9e7f12ce4a902fa14adc
Julian Kittel <62687014+ToniMacaroni@users.noreply.github.com>
2021-06-21 21:44:53 +0200

    support for CJD 1 and 2

--- SaberFactory/Game/CJDHandler.cs
+++ SaberFactory/Game/CJDHandler.cs
@@ -1,53 +1,142 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using SaberFactory.Helpers;
 
 namespace SaberFactory.Game
 {
     internal static class CJDHandler
     {
-        private static bool _gotCJDTypes;
-        private static MethodInfo _cjdAtMethod;
-        private static PropertyInfo _cjdLevelCustomDataType;
+        private static bool _initialized;
+        private static ICJDHandler _cjdHandler;
 
         public static bool GetField(this BeatmapData beatmap, string field, out object obj)
         {
-            try
+            if (!_initialized)
             {
-                if (!_gotCJDTypes)
-                {
-                    GetTypes();
-                }
-
-                if (_cjdAtMethod == null || _cjdLevelCustomDataType == null)
-                {
-                    obj = null;
-                    return false;
-                }
-
-                obj = _cjdAtMethod?.Invoke(null, new[] { _cjdLevelCustomDataType?.GetValue(beatmap), field });
-                if (obj == null) return false;
-                return true;
+                Init();
             }
-            catch (Exception)
+
+            if (_cjdHandler == null)
             {
                 obj = null;
                 return false;
             }
+
+            return _cjdHandler.GetField(beatmap, field, out obj);
         }
 
-        private static void GetTypes()
+        private static void Init()
         {
-            _gotCJDTypes = true;
+            _initialized = true;
 
             var pluginMetadata = IPA.Loader.PluginManager.GetPluginFromId("CustomJSONData");
             if (pluginMetadata == null) return;
 
-            var customBeatmapDataType = pluginMetadata.Assembly.GetType("CustomJSONData.CustomBeatmap.CustomBeatmapData");
+            if (pluginMetadata.Version.Major > 1)
+            {
+                _cjdHandler = new CJD2Handler();
+            }
+            else
+            {
+                _cjdHandler = new CJD1Handler();
+            }
 
-            var treesType = pluginMetadata.Assembly.GetType("CustomJSONData.Trees");
+            _cjdHandler.Setup(pluginMetadata.Assembly);
+        }
 
-            _cjdAtMethod = treesType?.GetMethod("at", BindingFlags.Static | BindingFlags.Public);
-            _cjdLevelCustomDataType = customBeatmapDataType?.GetProperty("levelCustomData", BindingFlags.Public | BindingFlags.Instance);
+        internal interface ICJDHandler
+        {
+            public bool GetField(BeatmapData beatmap, string field, out object obj);
+
+            public void Setup(Assembly assembly);
+        }
+
+        internal class CJD1Handler : ICJDHandler
+        {
+            private bool _isValid;
+
+            private MethodInfo _cjdAtMethod;
+            private PropertyInfo _cjdLevelCustomDataType;
+
+            bool ICJDHandler.GetField(BeatmapData beatmap, string field, out object obj)
+            {
+                try
+                {
+                    if (!_isValid)
+                    {
+                        obj = null;
+                        return false;
+                    }
+
+                    obj = _cjdAtMethod?.Invoke(null, new[] { _cjdLevelCustomDataType?.GetValue(beatmap), field });
+                    if (obj == null) return false;
+                    return true;
+                }
+                catch (Exception)
+                {
+                    obj = null;
+                    return false;
+                }
+            }
+
+            public void Setup(Assembly assembly)
+            {
+                var customBeatmapDataType = assembly.GetType("CustomJSONData.CustomBeatmap.CustomBeatmapData");
+
+                var treesType = assembly.GetType("CustomJSONData.Trees");
+
+                _cjdAtMethod = treesType?.GetMethod("at", BindingFlags.Static | BindingFlags.Public);
+                _cjdLevelCustomDataType = customBeatmapDataType?.GetProperty("levelCustomData", BindingFlags.Public | BindingFlags.Instance);
+
+                _isValid = _cjdAtMethod != null && _cjdLevelCustomDataType != null;
+            }
+        }
+
+        internal class CJD2Handler : ICJDHandler
+        {
+            private bool _isValid;
+
+            private PropertyInfo _cjdLevelCustomDataType;
+
+            bool ICJDHandler.GetField(BeatmapData beatmap, string field, out object obj)
+            {
+                try
+                {
+                    if (!_isValid)
+                    {
+                        obj = null;
+                        return false;
+                    }
+
+                    var dict = _cjdLevelCustomDataType.GetValue(beatmap) as Dictionary<string, object>;
+
+                    if (dict == null || !dict.TryGetValue(field, out obj))
+                    {
+                        obj = null;
+                        return false;
+                    }
+
+                    if (obj == null) return false;
+                    return true;
+                }
+                catch (Exception)
+                {
+                    obj = null;
+                    return false;
+                }
+            }
+
+            public void Setup(Assembly assembly)
+            {
+                var customBeatmapDataType = assembly.GetType("CustomJSONData.CustomBeatmap.CustomBeatmapData");
+
+                _cjdLevelCustomDataType = customBeatmapDataType?.GetProperty("levelCustomData", BindingFlags.Public | BindingFlags.Instance);
+
+                _isValid = _cjdLevelCustomDataType != null;
+            }
         }
     }
 }
